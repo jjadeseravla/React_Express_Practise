748c43113546693d906e66034c09532f4d2f4347
jjade seravla <jadealvares@hotmail.co.uk>
2018-07-19 10:04:18 +0100

    can now add item using redux and styled using react bootstrap

---
+++ client/src/components/ItemModal.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Form,
+  FormGroup,
+  Label,
+  Input
+} from 'reactstrap';
+import { connect } from 'react-redux';
+import { addItem } from '../actions/itemActions';
+
+class ItemModal extends Component {
+  state = {
+    modal: false,
+    name: '' //form that will have item name
+  }
+
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value}); //e.target.value gives you the value (whatever is typed in)
+  }
+
+  render() {
+    return(
+      <div>
+        <Button
+          color="dark"
+          style={{marginBottom: '2rem'}}
+          onClick={this.toggle}>
+          Add Item
+        </Button>
+
+        <Modal
+          isOpen={this.state.modal}
+          toggle={this.toggle}>
+          <ModalHeader toggle={this.toggle}>
+            Add to Shopping List
+          </ModalHeader>
+          <ModalBody>
+            <Form onSubmit={this.onSubmit}>
+              <FormGroup>
+                <Label for="item">Item</Label>
+                <Input
+                  type="text"
+                  name="name"
+                  id="item"
+                  placeholder="Add shopping item"
+                  onChange={this.onChange}>
+                  </Input>
+              </FormGroup>
+            </Form>
+          </ModalBody>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default connect()(ItemModal);
+
+//this is a container: a component that is hooked to redux.  If youre using redux state inside a react component, its called a container
